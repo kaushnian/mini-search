bab394cf53c7028c176b706e5d77c491f7308c24
Eugene Kaushnian <kefbox@gmail.com>
2022-06-19 17:03:42 -0400

    Handle errors and no resutls

--- Components/SearchResults.js
+++ Components/SearchResults.js
@@ -5,11 +5,15 @@ import useApi from '../hooks/useApi';
 export default function SearchResults({ query, searchApi }) {
   const { data, error } = useApi(query, searchApi);
 
+  if (!data) return;
+  if (error) return 'An error occurred while fetching the data';
+  if (data.length === 0) return 'No results';
+
   return (
     <List>
-      {data &&
-        !error &&
-        data.map(item => <SearchResultItem key={item.id} {...item} />)}
+      {data.map(item => (
+        <SearchResultItem key={item.id} {...item} />
+      ))}
     </List>
   );
 }
