4752907e231cd8e3931167a197c40ff4a1474850
Eugene Kaushnian <kefbox@gmail.com>
2022-06-19 00:10:52 -0400

    Use SWR to request data

--- Components/SearchResults.js
+++ Components/SearchResults.js
@@ -1,11 +1,27 @@
 import List from '@mui/material/List';
 import SearchResultItem from './SearchResultItem';
+import useSWR from 'swr';
+
+const fetcher = url => fetch(url).then(res => res.json());
+
+const key = 'key=U4DMV*8nvpm3EOpvf69Rxw((';
+const params = '&site=stackoverflow';
+const url = `https://api.stackexchange.com/2.3/search?${key}${params}&intitle=`;
 
 export default function SearchResults({ query }) {
+  const { data, isValidating } = useSWR(
+    query !== '' && `${url}${query}`,
+    fetcher
+  );
+
+  console.log(data);
+
   return (
-    <List>
-      <SearchResultItem />
-      <SearchResultItem />
-    </List>
+    <>
+      <List>
+        <SearchResultItem />
+        <SearchResultItem />
+      </List>
+    </>
   );
 }
